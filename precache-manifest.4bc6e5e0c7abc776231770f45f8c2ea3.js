b803d06f2430d461e34cad7d831cacff2da4c69b
jqly2 <jonathanly213@gmail.com>
2020-10-31 12:48:48 -0700

    Updates

---
+++ precache-manifest.4bc6e5e0c7abc776231770f45f8c2ea3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a013df137f316e75d5401594030d4ba2",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "1636b0524676ca1f5ff1",
+    "url": "/portfolio-site/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "97ea947e76f5490e710e",
+    "url": "/portfolio-site/static/css/main.9e5d31ae.chunk.css"
+  },
+  {
+    "revision": "1636b0524676ca1f5ff1",
+    "url": "/portfolio-site/static/js/2.951afaa4.chunk.js"
+  },
+  {
+    "revision": "83741e0b2a8dfa7427771959d460f889",
+    "url": "/portfolio-site/static/js/2.951afaa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ea947e76f5490e710e",
+    "url": "/portfolio-site/static/js/main.05956dc1.chunk.js"
+  },
+  {
+    "revision": "1eab4dbc6a425c409763",
+    "url": "/portfolio-site/static/js/runtime-main.9a1a40a9.js"
+  },
+  {
+    "revision": "982a1754ad8ca481d9d3e6addc97df41",
+    "url": "/portfolio-site/static/media/link.982a1754.png"
+  },
+  {
+    "revision": "98896218b122eb0df30a73a059874c66",
+    "url": "/portfolio-site/static/media/profile.98896218.jpg"
+  }
+]);
